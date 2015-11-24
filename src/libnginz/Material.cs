d1e4d58c822aa584be4c183cca08833f47eaa222
Splitty <splittydev@gmail.com>
2015-11-24 15:50:55 +0100

    Added a Material class
    Added empty ObjFileLoader class

---
+++ src/libnginz/Material.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using OpenTK.Graphics;
+
+namespace nginz
+{
+
+	/// <summary>
+	/// Material.
+	/// </summary>
+	public class Material
+	{
+
+		/// <summary>
+		/// The name of the material.
+		/// </summary>
+		readonly public string Name;
+
+		/// <summary>
+		/// The color of the ambient.
+		/// </summary>
+		public Color4 AmbientColor;
+
+		/// <summary>
+		/// The color of the diffuse.
+		/// </summary>
+		public Color4 DiffuseColor;
+
+		/// <summary>
+		/// The color of the specular.
+		/// </summary>
+		public Color4 SpecularColor;
+
+		/// <summary>
+		/// The specular coefficient.
+		/// </summary>
+		public float SpecularCoefficient;
+
+		/// <summary>
+		/// The transparency value.
+		/// </summary>
+		public float Transparency;
+
+		/// <summary>
+		/// The illumination model.
+		/// </summary>
+		public int IlluminationModel;
+
+		/// <summary>
+		/// The alpha texture map.
+		/// </summary>
+		public string AlphaTextureMap;
+
+		/// <summary>
+		/// The ambient texture map.
+		/// </summary>
+		public string AmbientTextureMap;
+
+		/// <summary>
+		/// The diffuse texture map.
+		/// </summary>
+		public string DiffuseTextureMap;
+
+		/// <summary>
+		/// The specular texture map.
+		/// </summary>
+		public string SpecularTextureMap;
+
+		/// <summary>
+		/// The specular highlight texture map.
+		/// </summary>
+		public string SpecularHighlightTextureMap;
+
+		/// <summary>
+		/// The displacement map.
+		/// </summary>
+		public string DisplacementMap;
+
+		/// <summary>
+		/// The stencil decal map.
+		/// </summary>
+		public string StencilDecalMap;
+
+		/// <summary>
+		/// The bump map.
+		/// </summary>
+		public string BumpMap;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="nginz.Material"/> class.
+		/// </summary>
+		/// <param name="name">Name.</param>
+		public Material (string name) {
+			Name = name;
+		}
+	}
+}
+
---
+++ src/libnginz/ModelLoaders/ObjFileLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace nginz
+{
+	public class ObjFileLoader
+	{
+		public ObjFileLoader () {
+		}
+	}
+}
+
