fba91448319f3aef37fee911df3d3dad6d46b3d1
Splitty <splittydev@gmail.com>
2015-11-28 23:01:08 +0100

    Added drawable and updatable interface Added SpriteBatch

---
+++ src/libnginz/Interfaces/IUpdatable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace nginz
+{
+
+	/// <summary>
+	/// Updatable interface.
+	/// </summary>
+	public interface IUpdatable
+	{
+
+		/// <summary>
+		/// Update.
+		/// </summary>
+		/// <param name="time">Time.</param>
+		void Update (GameTime time);
+	}
+}
+
