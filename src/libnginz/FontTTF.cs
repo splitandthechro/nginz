cc86fe8811e7e14c50007b5a1e96e607db6110c7
CallumDev <callum.mcging@gmail.com>
2015-12-05 21:41:29 +1030

    Freetype Work

---
+++ src/libnginz/FontTTF.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenTK;
+using SharpFont;
+namespace nginz
+{
+	public class Font
+	{
+		//Size of font textures (in pixels)
+		const int TEXTURE_SIZE = 512;
+
+		/// <summary>
+		/// Global FT_Library instance.
+		/// </summary>
+		static Library freetype;
+		static Font()
+		{
+			freetype = new Library ();
+		}
+
+		Face face;
+		public Font (string filename, float emSize) 
+		{
+			face = new Face (freetype, filename);
+			face.SetCharSize (new Fixed26Dot6 (emSize), new Fixed26Dot6(0), 96, 96);
+		}
+	}
+}
+
