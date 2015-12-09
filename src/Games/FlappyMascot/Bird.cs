d719f608dc7e01ea0f7ca3ee3a25924f38c566c6
Splitty <splittydev@gmail.com>
2015-12-09 04:31:42 +0100

    Updated FlappyMascot game

--- src/Games/FlappyMascot/Bird.cs
+++ src/Games/FlappyMascot/Bird.cs
@@ -7,11 +7,14 @@ namespace FlappyMascot
 {
 	public class Bird : IUpdatable, IDrawable2D
 	{
+		const float MAX_DRAG = 200;
+		const float MIN_DRAG = 50;
+		const float UP_VELOCITY = 125;
 		readonly Game game;
 		readonly SpriteSheet2D sheet;
 		readonly Animator animator;
-		float ypos, ydelta;
-		const float drag = 250;
+		float ypos, ydelta, upydelta;
+		float drag;
 		bool mouseDown;
 
 		public Bird (Game game) {
@@ -19,7 +22,6 @@ namespace FlappyMascot
 			var sheetTex = game.Content.Load<Texture2D> ("flappymascot_char.png", TextureConfiguration.Nearest);
 			sheet = new SpriteSheet2D (sheetTex, 4, 1);
 			animator = new Animator (sheet, 4);
-			animator.Scale = new Vector2 (1.5f, 1.5f);
 			animator.DurationInMilliseconds = 500;
 			animator.Position.X = Game.Resolution.Width * 0.25f;
 			ypos = (Game.Resolution.Height / 2) + (sheet [0].Height / 2);
@@ -29,14 +31,17 @@ namespace FlappyMascot
 
 		public void Update (GameTime time) {
 			ypos = MathHelper.Clamp (ypos + (drag * (float) time.Elapsed.TotalSeconds), (sheet [0].Height / 1.5f) / 2, Game.Resolution.Height - ((sheet [0].Height / 1.5f) * 2));
+			drag = MathHelper.Clamp (drag + (750 * (float)time.Elapsed.TotalSeconds), MIN_DRAG, MAX_DRAG);
 			if (ydelta > 0) {
 				var xdrag = (drag * 3 * (float) time.Elapsed.TotalSeconds);
 				ypos -= xdrag;
 				ydelta = MathHelper.Clamp (ydelta - xdrag, 0, Game.Resolution.Height);
 			}
 			if (game.Mouse.IsButtonDown (MouseButton.Left) && !mouseDown) {
-				ydelta += drag * .75f;
+				ydelta += UP_VELOCITY;
+				upydelta = ydelta;
 				mouseDown = true;
+				drag = MIN_DRAG;
 			} else if (game.Mouse.IsButtonUp (MouseButton.Left) && mouseDown)
 				mouseDown = false;
 			animator.Position.Y = ypos;
