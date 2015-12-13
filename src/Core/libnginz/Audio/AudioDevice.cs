36211e6d0b5f90fd9a7b8cf65703f06438c41bf7
CallumDev <callum.mcging@gmail.com>
2015-12-13 21:05:54 +1030

    Basic sfx code

--- src/Core/libnginz/Audio/AudioDevice.cs
+++ src/Core/libnginz/Audio/AudioDevice.cs
@@ -9,6 +9,9 @@ namespace nginz
 {
 	public class AudioDevice
 	{
+		//Maximum amount of concurrent sfx playbacks
+		const int MAX_SOURCES = 200;
+
 		internal AudioContext context;
 		internal bool ready = false;
 		bool createContext;
@@ -16,19 +19,55 @@ namespace nginz
 		//ConcurrentQueues to avoid threading errors
 		ConcurrentQueue<StreamingAudio> toRemove = new ConcurrentQueue<StreamingAudio> ();
 		ConcurrentQueue<StreamingAudio> toAdd = new ConcurrentQueue<StreamingAudio> ();
+		ConcurrentQueue<Action> actions = new ConcurrentQueue<Action>();
 		List<StreamingAudio> instances = new List<StreamingAudio> ();
+		Queue<int> sources = new Queue<int>();
+		List<int> playingSources = new List<int>();
+
+		Thread updateThread;
+
 		public AudioDevice(bool createContext = true)
 		{
 			this.createContext = createContext;
-			new Thread (new ThreadStart (UpdateThread)).Start ();
+			updateThread = new Thread (new ThreadStart (UpdateThread));
+			updateThread.Start ();
 		}
 
+		public void EnsureAudioThread(Action act)
+		{
+			bool done = false;
+			actions.Enqueue (() => {
+				act();
+				done = true;
+			});
+			while (!done)
+				Thread.Sleep (2);
+		}
 		void UpdateThread()
 		{
 			if(createContext)
 				context = new AudioContext ();
+			//Generate Sources
+			int[] generatedSources = new int[MAX_SOURCES];
+			AL.GenSources (generatedSources);
+			foreach (int src in generatedSources)
+				sources.Enqueue (src);
+			generatedSources = null;
+			//Let's get going
 			ready = true;
 			while (running) {
+				//Actions
+				Action action;
+				if (actions.TryDequeue (out action))
+					action ();
+				//Check sfx
+				for (int i = playingSources.Count - 1; i >= 0; i--) {
+					var src = playingSources [i];
+					if (AL.GetSourceState (src) != ALSourceState.Playing) {
+						sources.Enqueue (src);
+						playingSources.RemoveAt (i--);
+					}
+				}
 				//remove from items to update
 				while (toRemove.Count > 0) {
 					StreamingAudio item;
@@ -48,6 +87,14 @@ namespace nginz
 				Thread.Sleep (5);
 				//CheckALError ();
 			}
+			//We have stopped now
+			foreach(var src in playingSources) {
+				AL.SourceStop(src);
+				AL.DeleteSource(src);
+			}
+			foreach (var src in sources) {
+				AL.DeleteSource(src);
+			}
 		}
 		internal static void CheckALError()
 		{
@@ -63,9 +110,20 @@ namespace nginz
 		{
 			toRemove.Enqueue (audio);
 		}
+		internal void PlayBuffer(int buf, float volume)
+		{
+			actions.Enqueue (() => {
+				var src = sources.Dequeue();
+				AL.BindBufferToSource(src, buf);
+				AL.Source(src, ALSourcef.Gain, volume);
+				AL.SourcePlay(src);
+				playingSources.Add(src);
+			});
+		}
 		public void Dispose()
 		{
 			running = false;
+			updateThread.Join ();
 		}
 	}
 }
