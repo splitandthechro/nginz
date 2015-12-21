20624b7e0f6f9ee31b42a1a6fbc61e3216141551
Splitty <splittydev@gmail.com>
2015-12-21 11:48:56 +0100

    Reformatted assimp loader

--- src/Core/libnginz/Graphics/Models/ModelLoaders/AssimpLoader.cs
+++ src/Core/libnginz/Graphics/Models/ModelLoaders/AssimpLoader.cs
@@ -1,50 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Assimp;
 using Assimp.Configs;
 using nginz.Common;
 using OpenTK;
 using OpenTK.Graphics.OpenGL4;
 
-namespace nginz {
-	public class AssimpLoader : ICanLog {
+namespace nginz
+{
+
+	/// <summary>
+	/// Assimp loader.
+	/// </summary>
+	public static class AssimpLoader
+	{
+
+		/// <summary>
+		/// Load geometry data from a file.
+		/// </summary>
+		/// <returns>The geometry data.</returns>
+		/// <param name="path">Path.</param>
 		public static Dictionary<string, Geometry> LoadGeometry (string path) {
-			AssimpContext importer = new AssimpContext ();
-			NormalSmoothingAngleConfig config = new NormalSmoothingAngleConfig (66.0f);
+
+			// Create assimp context
+			var importer = new AssimpContext ();
+
+			// Create normal angle configuration
+			var config = new NormalSmoothingAngleConfig (66.0f);
 			importer.SetConfig (config);
 
-			LogStream logStream = new LogStream ((string msg, string userData) => {
-				Console.Write ("{0}", msg);
+			// Attach log stream
+			new LogStream ((msg, userData) => {
+				LogExtensions.LogStatic ("{0}", msg);
 				if (!string.IsNullOrEmpty (userData))
-					Console.WriteLine ("\t{0}", userData);
-		});
-			logStream.Attach ();
+					LogExtensions.LogStatic ("\t{0}", userData);
+			}).Attach ();
 
-			Scene scene = importer.ImportFile (path,
-												PostProcessSteps.CalculateTangentSpace |
-												PostProcessSteps.Triangulate |
-												PostProcessSteps.GenerateSmoothNormals);
-			Dictionary<string, Geometry> geometry = new Dictionary<string, Geometry> ();
+			// Set post process flags
+			var flags =
+				PostProcessSteps.CalculateTangentSpace
+				| PostProcessSteps.Triangulate
+				| PostProcessSteps.GenerateSmoothNormals;
 
+			// Import scene
+			var scene = importer.ImportFile (path, flags);
+
+			var geometry = new Dictionary<string, Geometry> ();
+
+			// Iterate over meshes
 			foreach (var mesh in scene.Meshes) {
-				List<Vector3> pos = new List<Vector3> ();
-				List<Vector2> tex = new List<Vector2> ();
-				List<Vector3> nrm = new List<Vector3> ();
+				var pos = new List<Vector3> ();
+				var tex = new List<Vector2> ();
+				var nrm = new List<Vector3> ();
 
+				// Iterate over face groups
 				foreach (var group in mesh.Faces) {
+
+					// Iterate over indices
 					foreach (var ind in group.Indices) {
 						pos.Add (mesh.Vertices[ind].ToVector3 ());
-						tex.Add (mesh.TextureCoordinateChannels[0][ind].ToVector3 ().ToVector2 ());
+						tex.Add (mesh.TextureCoordinateChannels [0][ind].ToVector3 ().ToVector2 ());
 						nrm.Add (mesh.Normals[ind].ToVector3 ());
 					}
 				}
 
-				geometry.Add (mesh.Name, new Geometry (BeginMode.Triangles)
-																.AddBuffer ("v_pos", pos.ToGLBuffer ())
-																.AddBuffer ("v_tex", tex.ToGLBuffer ())
-																.AddBuffer ("v_nrm", nrm.ToGLBuffer ()));
+				geometry.Add (
+					mesh.Name,
+					new Geometry (BeginMode.Triangles)
+						.AddBuffer ("v_pos", pos.ToGLBuffer ())
+						.AddBuffer ("v_tex", tex.ToGLBuffer ())
+						.AddBuffer ("v_nrm", nrm.ToGLBuffer ())
+				);
 			}
 
 			return geometry;
